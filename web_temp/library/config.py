3ea5498ae4134c4b608d31967e5934f9e1a50e04
acsii-63 <duy.nq203690@sis.hust.edu.vn>
2024-05-04 11:09:36 +0700

    rm temp dir, change control/stats topic method

--- web_temp/library/config.py
+++
@@ -1,8 +0,0 @@
-""" import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-SECRET_KEY = os.environ.get("KEY")
-SQUALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-SQUALCHEMY_TRACK_MODIFICATION = False """
